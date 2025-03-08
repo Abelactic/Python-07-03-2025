5faba273090a2805fc5699b9cf6c24fdb82b9248
Abelactic <israelbarrientos04@gmail.com>
2025-03-07 21:17:26 -0600

    Prueba de subida

--- Ejemplo1-08-03.py
+++ Ejemplo1-08-03.py
@@ -3,32 +3,29 @@ from PIL import Image, ImageTk
 
 root = tk.Tk()
 root.title("Animación GIF")
-root.configure(bg="#b1ff96")  # Color de fondo
+root.configure(bg="#b1ff96")
 
-# Cargar el GIF
+
 gif = Image.open("imagenes/bienvenida_git.gif")
 
-# Obtener todos los frames correctamente
+
 frames = []
 for i in range(gif.n_frames):
-    gif.seek(i)  # Moverse al frame i
-    frame = gif.copy()  # Hacer una copia del frame actual
-    frames.append(ImageTk.PhotoImage(frame))  # Convertir a formato Tkinter
+    gif.seek(i)
+    frame = gif.copy()
+    frames.append(ImageTk.PhotoImage(frame))
 
-# Crear un frame contenedor con fondo
+
 frame_container = tk.Frame(root, bg="#b1ff96")
 frame_container.pack()
 
-# Etiqueta con el mensaje
 message_label = tk.Label(frame_container, text="Gracias por descargarme :)",
-                         font=("Arial", 14, "bold"), bg="#b1ff96")
+                         font=("Arial", 20, "bold"), bg="#b1ff96")
 message_label.pack()
 
-# Etiqueta para la animación del GIF
+
 gif_label = tk.Label(frame_container, bg="#b1ff96")
 gif_label.pack()
-
-# Función para actualizar la animación
 
 
 def update(ind):
@@ -37,7 +34,6 @@ def update(ind):
     root.after(100, update, (ind+1) % len(frames))  # 100 ms por frame
 
 
-# Iniciar la animación
 update(0)
 
 root.mainloop()
