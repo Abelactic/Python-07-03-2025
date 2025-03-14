3e971fcf3bd658e043f0a924ac206e3cf09d9246
Abelactic <israelbarrientos04@gmail.com>
2025-03-13 21:07:39 -0600

    Solucion a rutas relativas

--- Ejemplo1-08-03.py
+++ Ejemplo1-08-03.py
@@ -1,21 +1,26 @@
+import os
 import tkinter as tk
 from PIL import Image, ImageTk
+
+# Obtiene la ruta del script en ejecución
+base_dir = os.path.dirname(os.path.abspath(__file__))
+image_path = os.path.join(base_dir, "imagenes", "bienvenida_git.gif")
 
 root = tk.Tk()
 root.title("Animación GIF")
 root.configure(bg="#b1ff96")
 
+# Cargar el GIF
+gif = Image.open(image_path)
 
-gif = Image.open("imagenes/bienvenida_git.gif")
-
-
+# Extraer los frames de la animación
 frames = []
 for i in range(gif.n_frames):
     gif.seek(i)
     frame = gif.copy()
     frames.append(ImageTk.PhotoImage(frame))
 
-
+# Crear los elementos de la interfaz
 frame_container = tk.Frame(root, bg="#b1ff96")
 frame_container.pack()
 
@@ -23,9 +28,10 @@ message_label = tk.Label(frame_container, text="Gracias por descargarme :)",
                          font=("Arial", 20, "bold"), bg="#b1ff96")
 message_label.pack()
 
-
 gif_label = tk.Label(frame_container, bg="#b1ff96")
 gif_label.pack()
+
+# Función para actualizar la animación
 
 
 def update(ind):
